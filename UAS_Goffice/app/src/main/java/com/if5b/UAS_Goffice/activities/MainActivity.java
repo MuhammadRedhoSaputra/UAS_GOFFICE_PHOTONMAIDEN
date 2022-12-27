c52dbe37bdabe200bc94b14e4dbe3cd4c13a1eab
MuhammadRedhoSaputra <102501722+MuhammadRedhoSaputra@users.noreply.github.com>
2022-12-27 22:29:04 +0700

    intro slider fix

---
+++ UAS_Goffice/app/src/main/java/com/if5b/UAS_Goffice/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.if5b.UAS_Goffice.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.if5b.UAS_Goffice.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
