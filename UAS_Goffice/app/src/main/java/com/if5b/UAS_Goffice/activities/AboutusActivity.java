402981b20544ea37d81259e5eec939366db9a816
MuhammadRedhoSaputra <102501722+MuhammadRedhoSaputra@users.noreply.github.com>
2023-01-07 21:40:48 +0700

    Membuat image glider + Aboutus

---
+++ UAS_Goffice/app/src/main/java/com/if5b/UAS_Goffice/activities/AboutusActivity.java
@@ -0,0 +1,32 @@
+package com.if5b.UAS_Goffice.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.if5b.UAS_Goffice.R;
+
+import java.util.ArrayList;
+
+public class AboutusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+
+        ImageSlider imageSlider = findViewById(R.id.is_about);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.image1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image2, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image3, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.image4, ScaleTypes.FIT));
+
+        imageSlider.setImageList(slideModels, ScaleTypes.FIT);
+
+    }
+}
--- UAS_Goffice/app/src/main/java/com/if5b/UAS_Goffice/fragment/ProfileFragment.java
+++ UAS_Goffice/app/src/main/java/com/if5b/UAS_Goffice/fragment/ProfileFragment.java
@@ -2,15 +2,22 @@ package com.if5b.UAS_Goffice.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.if5b.UAS_Goffice.R;
+import com.if5b.UAS_Goffice.activities.AboutusActivity;
+import com.if5b.UAS_Goffice.activities.IzinActivity;
+import com.if5b.UAS_Goffice.activities.KeluarActivity;
 import com.if5b.UAS_Goffice.activities.LoginActivity;
+import com.if5b.UAS_Goffice.activities.MasukActivity;
 import com.if5b.UAS_Goffice.activities.ProfileActivity;
 
 import java.util.ArrayList;
@@ -25,7 +32,7 @@ public class ProfileFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private CardView button1, button2;
     private String mParam1;
     private String mParam2;
 
@@ -49,6 +56,41 @@ public class ProfileFragment extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        button1 = rootView.findViewById(R.id.button1);
+        button2 = rootView.findViewById(R.id.button2);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), AboutusActivity.class);
+                startActivity(intent);
+            }
+        });
+        return rootView;
     }
 
 }
