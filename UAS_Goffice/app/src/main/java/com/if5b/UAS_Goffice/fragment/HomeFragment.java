8920e17bc3c5afbdf23bcc4262a6d202e5aea5dd
MuhammadRedhoSaputra <102501722+MuhammadRedhoSaputra@users.noreply.github.com>
2023-01-07 17:22:57 +0700

    delete

--- UAS_Goffice/app/src/main/java/com/if5b/UAS_Goffice/fragment/HomeFragment.java
+++
@@ -1,6 +0,0 @@
-package com.if5b.UAS_Goffice.fragment;
-
-import androidx.fragment.app.Fragment;
-
-public class HomeFragment extends Fragment {
-}
--- UAS_Goffice/app/src/main/java/com/if5b/UAS_Goffice/fragment/ProfileFragment.java
+++
@@ -1,7 +0,0 @@
-package com.if5b.UAS_Goffice.fragment;
-
-import androidx.fragment.app.Fragment;
-
-public class ProfileFragment extends Fragment {
-
-}
